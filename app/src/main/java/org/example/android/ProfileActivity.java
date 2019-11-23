765a394a4bdec61bc79ab7196f193bc343e1ce86
Anton Shpakovsky <shpakovsky86@gmail.com>
2019-11-23 18:31:03 +0300

    Improved profile activity

--- app/src/main/java/org/example/android/ProfileActivity.java
+++ app/src/main/java/org/example/android/ProfileActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -19,13 +23,26 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle("Alice Smith");
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_bookmark :
+                Toast.makeText(getApplicationContext(), "Add to bookmark", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/org/example/android/ResourceLink.java
+++ app/src/main/java/org/example/android/ResourceLink.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,29 +13,73 @@ import androidx.annotation.Nullable;
 
 public class ResourceLink extends LinearLayout {
     private static final String TAG = "ResourceLink";
-    private int attrIcon = 0;
-    private String attrText = "";
+    private int icon;
+    private String label;
     private ImageView linkIcon;
-    private TextView linkText;
+    private TextView linkLabel;
 
+    /**
+     * ResourceLink constructor
+     *
+     * @param context
+     * @param attrs
+     */
     public ResourceLink(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        // Inflate view
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.view_resource_link, this);
+        // Get views
         linkIcon = findViewById(R.id.linkIcon);
-        linkText = findViewById(R.id.linkText);
+        linkLabel = findViewById(R.id.linkText);
+        // Get view properties from layout
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(
                 attrs, R.styleable.ResourceLink, 0, 0);
         try {
-            attrIcon = typedArray.getResourceId(R.styleable.ResourceLink_linkIcon, 0);
-            attrText = typedArray.getString(R.styleable.ResourceLink_linkText);
-            linkIcon.setImageResource(attrIcon);
-            linkText.setText(attrText);
+            icon = typedArray.getResourceId(R.styleable.ResourceLink_linkIcon, R.drawable.ic_link_black_24dp);
+            label = typedArray.getString(R.styleable.ResourceLink_linkText);
+            linkIcon.setImageResource(icon);
+            linkLabel.setText(label);
         } catch (Exception ex) {
             Log.e(TAG, ex.getMessage());
         } finally {
             typedArray.recycle();
         }
+    }
 
+    /**
+     * Icon setter
+     *
+     * @param resource icon resource id
+     */
+    public void setIcon(int resource) {
+        linkIcon.setImageResource(resource);
+    }
+
+    /**
+     * Label setter
+     *
+     * @param label label text
+     */
+    public void setLinkLabel(String label) {
+        linkLabel.setText(label);
+    }
+
+    /**
+     * Label getter
+     *
+     * @return value of link label
+     */
+    public String getLinkLabel() {
+        return label;
+    }
+
+    /**
+     * Icon getter
+     *
+     * @return current icon value
+     */
+    public int getIcon() {
+        return icon;
     }
 }
